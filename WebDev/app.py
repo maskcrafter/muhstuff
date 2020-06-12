b6ebdd6facd0306c90e4d49d22fce1940434dfda
harry <sanmiguella@hotmail.com>
2020-06-12 09:39:50 +0800

    update

--- WebDev/app.py
+++ WebDev/app.py
@@ -10,7 +10,8 @@ WEEKDAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", 
 CURRENT_DAY = day_name[date.today().weekday()]
 
 food_dict = dict()
-order_dict = dict()
+food_cart_dict = dict()
+food_menu_of_the_day_dict = dict()
 
 app = Flask(__name__)
 
@@ -25,38 +26,45 @@ def home():
 
 @app.route('/view/<day_of_the_week>')
 def view(day_of_the_week):
-    food_menu = food_dict.get(day_of_the_week)
+    food_menu_dict = food_dict.get(day_of_the_week)
 
     # food_menu will be None if key(day_of_the_week) is non-existent
-    if food_menu:  # True
-        return render_template('data.html', food_data=food_menu, day_of_the_week=day_of_the_week)
+    if food_menu_dict:  # True
+        return render_template('data.html', food_data = food_menu_dict, day_of_the_week = day_of_the_week)
     else:
-        return render_template('404.html', message=f'/view/{day_of_the_week} was not found.')
+        return render_template('404.html', message = f'/view/{day_of_the_week} was not found.')
 
 @app.route('/order/form')   # 1st -> order_food.html 
 def form():
-    food_menu_of_the_day_dict = food_dict[CURRENT_DAY]
+    refresh_data()
     return render_template('order_food.html', food_data = food_menu_of_the_day_dict, current_day = CURRENT_DAY)
 
 @app.route('/order/cart')   # 2nd -> Form action submitted to /order/cart
 def order_food():
-    global order_dict
-    order_dict.clear()
+    global food_cart_dict
+    food_cart_dict.clear()
 
     order_number = randint(1, 500)
-    get_params = request.args
+    get_parameters = request.args
 
-    for food_name in get_params:
-        food_quantity = get_params[food_name]
+    for food_name in get_parameters:
+        try:
+            food_quantity = int(get_parameters.get(food_name))
 
-        if food_quantity != '':
-            order_dict[food_name] = int(food_quantity)
+            if food_quantity > 0:
+                food_price = food_menu_of_the_day_dict.get(food_name)
+                price_and_quantity_list = [food_price, food_quantity]
+                food_cart_dict[food_name] = price_and_quantity_list
+        
+        # ValueError happens when quantity field is blank. Will do nothing and goes to the next loop.
+        except ValueError:
+            pass           
 
-    return redirect(url_for('cart', order_number=order_number))
+    return redirect(url_for('cart', order_number = order_number))
 
 @app.route('/order/<order_number>') # 3rd -> Redirected to order number
 def cart(order_number):
-    return render_template('cart.html', order_number = order_number, order_dict = order_dict)
+    return render_template('cart.html', order_number = order_number, food_cart_dict = food_cart_dict)
 
 def load_data_to_nested_dict():
     temp_food_dict = dict()
@@ -86,6 +94,13 @@ def load_data_to_nested_dict():
         print(f"Could not open -> {path_to_data}")
         sys.exit(1)
 
-if __name__ == '__main__':
+def refresh_data():
+    global food_dict
+    global food_menu_of_the_day_dict
+
     food_dict = load_data_to_nested_dict()
+    food_menu_of_the_day_dict = food_dict.get(CURRENT_DAY)
+
+if __name__ == '__main__':
+    refresh_data()
     app.run(debug = True)
