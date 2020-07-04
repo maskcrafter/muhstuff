9a1ffa8a45ef5076e1dc83fbf9a4cbf6048b4bca
maskcrafter <escuse_me_mr@hotmail.com>
2020-07-04 22:37:12 +0800

    reshuffle

--- sockets/sqlite_creds.sql
+++
@@ -1,7 +0,0 @@
-create table [credentials] (
-	[user_id] integer primary key,
-	[username] text not null unique,
-	[password] text not null,
-	[is_admin] text not null,
-	[discount] text not null
-);
